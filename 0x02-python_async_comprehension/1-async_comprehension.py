696e0746409464b0179a163339fe9b48e273ad2b
FiraoolTulu <117259416+firaoltulu@users.noreply.github.com>
2024-04-23 22:13:38 +0300

    my first commit

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Task 1: Async Comprehensions."""
+
+import typing
+
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> typing.List[float]:
+    """This Function Creates a list of 10 numbers from a 10-number generator."""
+    return [rand async for rand in async_generator()]
